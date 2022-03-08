552493fa6f1e846b11655c7c722946dfaabc2cf6
Sahil <canshu073@gmail.com>
2022-03-08 18:50:56 +0530

    module2

---
+++ module 2/intro.js
@@ -0,0 +1,28 @@
+//variable
+
+console.log("hellow")
+let num =10;
+console.log(num);
+let char = "a";
+console.log(char);
+let str ="string is here";
+console.log(str);
+let bool = true;
+console.log(bool);
+for(i=1;i<=5;i++){
+    console.log(i);
+}
+let n=7;
+let isprime = true;
+for(let i=2;i*i<=n;i++){
+if(n%i == 0){
+    isprime =false;
+}
+}if (isprime == true){
+console.log("number is prime");
+
+    
+} else {
+    console.log("number is notprime");
+    
+}
