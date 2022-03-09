dd898cf3bf210396a20743c84483704429fd12c4
Sahil <canshu073@gmail.com>
2022-03-09 17:21:20 +0530

    string

---
+++ module 2/string.js
@@ -0,0 +1,32 @@
+let str = "i am a string";
+console.log(str);
+
+//length of string
+ let len = str.length;
+ console.log(str);
+
+ //sliced method
+ let slicedarry = str.slice(2,8);
+ console.log(slicedarry);
+
+ //replace method
+ let replacestr = str.replace(" am " ," was");
+ console.log(replacestr);
+
+ //upper case
+ let uppercase = str.toUpperCase();
+ console.log(uppercase);
+
+ //lower case
+ let lowercase = str.toLowerCase();
+ console.log(lowercase);
+
+ //concate
+ let firstname ="sahil";
+ let lastname = "chaudhary";
+ let fullname = firstname+lastname;
+ console.log(fullname);
+
+ //split-->
+ let splitted = str.split("  ");
+ console.log(splitted);
