4de970fd288ff9bb0c39a74d90522b1931bb187e
Sahil <canshu073@gmail.com>
2022-03-11 21:50:13 +0530

    object in js

---
+++ module 2/object.js
@@ -0,0 +1,26 @@
+//empty object
+ //let obj ={}
+   // console.log(obj);
+     
+//let objsahil ={
+  //  name: "SAHIL",
+    //Age: 22,
+    //Phnumber: 9882567121,
+    //Lastname: 'chaudhary'
+//};    
+//console.log(objsahil);
+
+let capamrica ={
+    Name: 'steve',
+    Age: 9999,
+    friend: ['natsha','bucky','bucky','thor'],
+    adress:{
+        state:'himachal',
+        city:'kangra',
+    },
+    sayhi:function(){
+        console.log("say hii heloo anshu");
+    }
+
+}
+console.log(capamrica);
