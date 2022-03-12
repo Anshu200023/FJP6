ca6f50a8f5644d3b487cac6d1539c9fc19df6571
Sahil <canshu073@gmail.com>
2022-03-12 21:47:36 +0530

    childprocess

--- module 2/node/childprocess.js
+++ module 2/node/childprocess.js
@@ -1,7 +1,9 @@
 let cp = require('child_process');
-console.log(cp);
+//console.log(cp);
 //
 //
 //open  calculator with the help of file
 //
-    cp.execFileSync("calc")
+    //cp.execFileSync("calc")
+    let content = cp.execSync("node text.js");
+    console.log(""+content);
