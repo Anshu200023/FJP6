bdbd00c5d217d0f15006a82233bb28111f1dd30b
Sahil <canshu073@gmail.com>
2022-03-13 13:57:32 +0530

    write

--- module 2/node/fs.js
+++ module 2/node/fs.js
@@ -2,5 +2,20 @@ let fs = require("fs");
 //console.log(fs);
 let path = require("path");
 let filepath = path.join(__dirname,"file.txt");
-console.log(filepath);
+//console.log(filepath);
+// c-create
 fs.writeFileSync(filepath,"hello file path");
+
+//r- read
+console.log('before update:->');
+
+let content = fs.readFileSync(filepath,'utf-8');
+console.log(content);
+
+//u-update 
+fs.appendFileSync(filepath,'\nNew added content');
+console.log('after update:->');
+console.log(fs.readFileSync(filepath,'utf-8'));
+
+//D - delete
+ fs.unlinkSync(filepath);
