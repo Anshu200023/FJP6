6886305d85c805e87b6ecc8b6dec1081ea7a40c5
Sahil <canshu073@gmail.com>
2022-03-15 22:37:20 +0530

    solved q1 and q2

--- module 2/node/fs.js
+++ module 2/node/fs.js
@@ -4,12 +4,13 @@ let path = require("path");
 //let filepath = path.join(__dirname,"file.txt");
 //console.log(filepath);
 // c-create
-//fs.writeFileSync(filepath,"hello file path");
+//fs.writeFileSync(filepath,"anshu");
 
 //r- read
 //console.log('before update:->');
 
 //let content = fs.readFileSync(filepath,'utf-8');
+
 //console.log(content);
 
 //u-update 
@@ -18,23 +19,23 @@ let path = require("path");
 //console.log(fs.readFileSync(filepath,'utf-8'));
 
 //D - delete
- //fs.unlinkSync(filepath);
+ fs.unlinkSync(filepath);
  //create directory
- if(!fs.existsSync("hamrri directory"))
- fs.mkdirSync("hamrri directory");
+ //if(!fs.existsSync("hamrri directory"))
+ //fs.mkdirSync("hamrri directory");
 
  //read directory
- let folderpath = "C:\\Users\\Dell\\Desktop\\FJP6\\module 1";
+ //let folderpath = "C:\\Users\\Dell\\Desktop\\FJP6\\module 1";
 
- let contentofFolder = fs.readdirSync(folderpath);
- console.log(contentofFolder);
+ //let contentofFolder = fs.readdirSync(folderpath);
+ //console.log(contentofFolder);
 
  //delete a directory
- fs.rmdirSync("hamrri directory");
+ //fs.rmdirSync("hamrri directory");
  //
  // copy  a file
- let sourcepath = path.join(__dirname,"file.txt");
- let destination = path.join(__dirname,"module","file.txt");
+ let sourcepath = path.join(__dirname,"index.html");
+ let destination = path.join(__dirname,"module2","index.html");
  console.log(sourcepath);
  console.log(destination);
  fs.copyFileSync(sourcepath,destination);;
