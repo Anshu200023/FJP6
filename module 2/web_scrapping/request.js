22897fcee78f5bf085e6ad5e20460a7c45b676c6
Sahil <canshu073@gmail.com>
2022-03-30 22:33:47 +0530

    jsdom request activity

--- module 2/web_scrapping/request.js
+++ module 2/web_scrapping/request.js
@@ -1,11 +1,13 @@
 const request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if(error)
 
-    
-  console.error('error:', error); // Print the error if one occurred
-  else{
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  }
-});
+
+const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard"
+request(link, cb);
+
+function cb(error, response, body) {
+    if(error)
+        console.error('error:', error); // Print the error if one occurred
+    else{
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    }
+}
